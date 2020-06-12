347274ece6c391b1c855b7dcc449ab24911f9cce
EleganteVagbond <hpogt1@gmail.com>
2020-06-12 15:56:24 -0400

    swap paperclip for activerecord

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,7 +4,8 @@ module ArticlesHelper
   # to use in the ArticleController be sure to include this module!
   private
   def article_params
-    params.require(:article).permit(:title,:body, :tag_list)
+    # tag list is the set of tags (M2M) and image is the paperclip image file
+    params.require(:article).permit(:title,:body, :tag_list, :image)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,12 @@ class Article < ApplicationRecord
 
   has_many :tags, through: :taggings
 
+  # tell ActiveStorage that this object has images attached to it
+  has_one_attached :image
+  # specify which attachments are valid (could include other image types)
+  # validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
+  validates :image, content_type:  ['image/jpg', 'image/jpeg', 'image/png']
+
   def tag_list
     self.tags.collect do |tag| 
       tag.name
@@ -15,4 +21,5 @@ class Article < ApplicationRecord
     new_or_found_tags = tag_arr.collect {|tag_name| Tag.find_or_create_by(name: tag_name)}
     self.tags = new_or_found_tags
   end
+
 end
