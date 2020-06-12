5c02223e39566aea4d0fb795c37394ef0d5e782f
EleganteVagbond <hpogt1@gmail.com>
2020-06-12 07:14:44 -0400

    Adding CRUD functionality for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   
   def index
     @articles = Article.all
@@ -6,6 +7,44 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(params[:article])
+    # create and save/persist the article
+    @article = Article.new(article_params)
+    @article.save
+    # notify the user that it succeeded
+    flash.notice= "Article '#{@article.title}' created. Good job, you!'"
+    # now show the saved article
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+    @article.save
+    flash.notice= "Article '#{@article.title}' deleted :'( Fare thee well article '"
+
+    redirect_to article_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    # update implicitly calls save
+    @article.update(article_params)
+
+    flash.notice= "Article '#{@article.title}' updated!'"
+
+    redirect_to article_path
   end
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+
+  # require a param called article and associated params title and body
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
 end
